1a4423b0cdd92cf00656ba36cf7956665dbd4a7b
Abhishek Sharma <akstenx@gmail.com>
2024-10-18 18:59:12 +0530

    BD1.4_HW_1 completed

--- index.js
+++ index.js
@@ -4,11 +4,98 @@ const port = 3000
 
 // Function to return a welcome message
 function getWelcomeMessage() {
-  return "Welcome to our service!"
+  return "We will now learn functions!"
 }
 // Endpoint 1: Return a welcome message
 app.get("/welcome", (req, res) => {
   res.send(getWelcomeMessage())
+})
+
+// Function to return a greeting message
+function getGreetingMessage(username) {
+  return "Hey, " + username + "! Are you ready to learn functions with us?"
+}
+// Endpoint 2: Take a username and return a greeting message
+app.get("/greet", (req, res) => {
+  let username = req.query.username
+  res.send(getGreetingMessage(username))
+})
+
+// Function to check if a person has
+function checkYearsOfExp(yearsOfExp) {
+  if (yearsOfExp > 0) {
+    return "You have some experience with functions. Great!"
+  } else {
+    return "No worries. You wilt start writing functions in no time!"
+  }
+}
+// Endpoint 3: Take the years of experience in functions and return a message
+app.get("/message", (req, res) => {
+  let yearsOfExp = parseFloat(req.query.yearsOfExp)
+  res.send(checkYearsOfExp(yearsOfExp))
+})
+
+// Function to return the time the student can dedicate to learn functions
+function getTime(days, hours) {
+  return days * hours
+}
+// Endpoint 4: Take the number of days and hours a student can dedicate to learn functions per week and returns the total hours available per week.
+app.get("/hours", (req, res) => {
+  let days = parseFloat(req.query.days)
+  let hours = parseFloat(req.query.hours)
+  res.send(getTime(days, hours).toString())
+})
+
+// Function to return the modules completion message
+function getModuteComptetion(username, hasCompleted) {
+  if (hasCompleted) {
+    return username + " has completed the modules"
+  } else {
+    return username + " has not completed the modules"
+  }
+}
+// Endpoint 5: Take a username and a boolean indicating module completion status, return a message if the student has completed the modules or not
+app.get("/module-completion-status", (req, res) => {
+  let username = req.query.username
+  let hasCompleted = req.query.hasCompleted === "true"
+  res.send(getModuteComptetion(username, hasCompleted))
+})
+
+// Function to return a personalized greeting message
+function getPersonatizedGreeting(city, name) {
+  return "Hey, " + name + "!What's famous about " + city + "?"
+}
+// Endpoint 6: Take a student's city and name, return a personalized greeting message
+app.get("/personalized-greeting", (req, res) => {
+  let city = req.query.city
+  let name = req.query.name
+  res.send(getPersonatizedGreeting(city, name))
+})
+
+// Function to find the age from birth year
+function findAge(birthyear) {
+  return 2024 - birthyear
+}
+// Endpoint 7: Take the birth year of the student and return the age
+app.get("/find-age", (req, res) => {
+  let birthyear = parseInt(req.query.birthyear)
+  res.send(findAge(birthyear).toString())
+})
+
+// Function to return the time required message
+function findRequiredTime(days, hours) {
+  let time = days * hours
+  if (time >= 30) {
+    return "The time being dedicated is sufficient for learning functions"
+  } else {
+    return "The time being dedicated is not sufficient for learning functions"
+  }
+}
+// Endpoint 8: Take the days per week and hours per day a student can dedicate to learn functions and return whether it is sufficient ( 30)
+app.get("/is-time-sufficient", (req, res) => {
+  let days = parseFloat(req.query.days)
+  let hours = parseFloat(req.query.hours)
+  res.send(findRequiredTime(days, hours).toString())
 })
 
 app.listen(port, () => {
